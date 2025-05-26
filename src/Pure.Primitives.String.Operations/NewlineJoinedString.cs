8f5c37feacdfd29c82ac34a4e99061bcce290569
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-26 12:02:57 +0300

    Improved value handling in NewLineJoinedString

--- src/Pure.Primitives.String.Operations/NewlineJoinedString.cs
+++ src/Pure.Primitives.String.Operations/NewlineJoinedString.cs
@@ -4,25 +4,16 @@ namespace Pure.Primitives.String.Operations;
 
 public sealed record NewLineJoinedString : IString
 {
-    private readonly IString _separator;
+    private readonly IString _joinedString;
 
     public NewLineJoinedString(params IString[] values) : this(values.AsReadOnly()) { }
 
     public NewLineJoinedString(IEnumerable<IString> values)
     {
-        _separator = new NewLineString();
-        _values = values;
+        _joinedString = new JoinedString(new NewLineString(), values);
     }
 
-    string IString.Value
-    {
-        get
-        {
-            IString joinedString = new JoinedString(_separator, _values);
-
-            return joinedString.Value;
-        }
-    }
+    string IString.Value => _joinedString.Value;
 
     public override int GetHashCode()
     {
