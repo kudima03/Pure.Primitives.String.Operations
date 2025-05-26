3eaaa5a0186d144888bd14d8a68205d7b6b5fb17
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-26 14:08:34 +0300

    Implemented NotEqualCondition

---
+++ src/Pure.Primitives.String.Operations/NotEqualCondition.cs
@@ -0,0 +1,39 @@
+﻿using Pure.Primitives.Abstractions.Bool;
+using Pure.Primitives.Abstractions.String;
+
+namespace Pure.Primitives.String.Operations;
+
+public sealed record NotEqualCondition : IBool
+{
+    private readonly IEnumerable<IString> _values;
+
+    public NotEqualCondition(params IString[] values) : this(values.AsReadOnly()) { }
+
+    public NotEqualCondition(IEnumerable<IString> values)
+    {
+        _values = values;
+    }
+
+    bool IBool.Value
+    {
+        get
+        {
+            if (_values.Take(2).Count() < 2)
+            {
+                throw new ArgumentException();
+            }
+
+            return _values.DistinctBy(x => x.Value).Count() > 1;
+        }
+    }
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
