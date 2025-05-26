1e6f968b9820a394a0cafddce4c796d54d7895b0
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-26 10:11:43 +0300

    Implemented WhitespaceJoinedString.

---
+++ src/Pure.Primitives.String.Operations/WhitespaceJoinedString.cs
@@ -0,0 +1,39 @@
+﻿using Pure.Primitives.Abstractions.String;
+
+namespace Pure.Primitives.String.Operations;
+
+public sealed record WhitespaceJoinedString : IString
+{
+    private readonly IString _separator;
+
+    private readonly IEnumerable<IString> _values;
+
+    public WhitespaceJoinedString(IEnumerable<IString> values)
+    {
+        _separator = new WhitespaceString();
+        _values = values;
+    }
+
+    string IString.Value
+    {
+        get
+        {
+            if (!_values.Any())
+            {
+                throw new ArgumentException();
+            }
+
+            return string.Join(_separator.Value, _values.Select(x => x.Value));
+        }
+    }
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
