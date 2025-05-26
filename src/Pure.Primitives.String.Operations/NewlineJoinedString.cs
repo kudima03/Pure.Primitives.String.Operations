a886de146532b66a8e2607a57453d587f4292a0e
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-26 10:22:58 +0300

    Implemented NewLineJoinedString.

---
+++ src/Pure.Primitives.String.Operations/NewlineJoinedString.cs
@@ -0,0 +1,36 @@
+﻿using Pure.Primitives.Abstractions.String;
+
+namespace Pure.Primitives.String.Operations;
+
+public sealed record NewLineJoinedString : IString
+{
+    private readonly IString _separator;
+
+    private readonly IEnumerable<IString> _values;
+
+    public NewLineJoinedString(IEnumerable<IString> values)
+    {
+        _separator = new NewLineString();
+        _values = values;
+    }
+
+    string IString.Value
+    {
+        get
+        {
+            IString joinedString = new JoinedString(_separator, _values);
+
+            return joinedString.Value;
+        }
+    }
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
